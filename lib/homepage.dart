c19ce29a1432f5ecf5193e623e4483287f1b3229
Amit Kumar <amitgarg.cse@gmail.com>
2021-05-01 20:14:33 +0530

    Added Page header

---
+++ lib/homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 80,
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  'Prachet Infotech Pvt. Ltd.',
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                        fontSize: 26,
+                        color: Color.fromRGBO(252, 183, 7, 1),
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1
+                    )
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
